95182ce783cd2d361df77744d314e4535728ecc6
Prem Pratap Singh <pratapprem88@gmail.com>
2020-07-09 23:39:31 +0530

    Bar graph

--- bar.py
+++ bar.py
@@ -4,6 +4,7 @@ import mpld3
 plt.bar([1,3,5,7,9],[5,2,7,8,2],label="First Group")
 plt.bar([3,4,6,8,10],[8,6,2,5,6],label="Second Group")
 plt.legend()
+#giving labels to axis
 plt.ylabel("Height")
 plt.xlabel("Number")
 plt.title("Bar Graph")
